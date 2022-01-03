11cb6b9b27680dc516e4eccb35ad1d0d41c345de
ELCHAKIR <92917452+Mohamed-Anass-Riyadi@users.noreply.github.com>
2022-01-03 10:06:41 +0100

    Add

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Riyadi Anass.", "Designer.", "Developer.", "Freelancer."],
+      strings: ["Riyadi Anass.", "Developer.",],
       typeSpeed: 100,
       loop: true,
     });
